c2ee8d76f01b22209857000c5c2c31299d504ccf
Andy Gocke <agocke@gmail.com>
2012-03-06 05:12:59 -0600

    Changed to use filter streaming api

--- twitter_search.py
+++ twitter_search.py
@@ -1,9 +1,32 @@
-
 import json
+import pycurl
 import urllib
-import urllib2
 
-def search_twitter(search_url_string='http://search.twitter.com/search.json?q={}'):
-  search_json = urllib2.urlopen(SEARCH_URL_STRING.format(urllib.quote('primaries vote')))
-  search_decode = json.loads(search_json.read())
-  return search_decode
+class TwitterStreamClient(object):
+  def __init__(self, topics, callback):
+    with open('twitter_auth.json', 'r') as auth_f:
+        auth = json.load(auth_f)
+    self.callback = callback
+    self.buf = ''
+
+    conn = pycurl.Curl()
+    conn.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
+    conn.setopt(pycurl.USERPWD, '{}:{}'.format(auth[u'user'],
+                                               auth[u'password']))
+    conn.setopt(pycurl.URL, 'https://stream.twitter.com/1/statuses/filter.json')
+    conn.setopt(pycurl.POSTFIELDS, urllib.urlencode(dict(track=topics)))
+    conn.setopt(pycurl.WRITEFUNCTION, self.got_data)
+    conn.perform()
+
+  def got_data(self, data):
+    self.buf += data
+    if data.endswith('\r\n') and self.buf.strip():
+      content = json.loads(self.buf)
+      self.buf = ''
+      self.callback(content)
+
+
+if __name__ == '__main__':
+  def callback(content):
+    print content
+  client = TwitterStreamClient('primary', callback)
