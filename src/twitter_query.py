31a0cc4c7655226aec70637249edf17153138639
Andy Gocke <agocke@gmail.com>
2012-03-14 19:50:59 -0500

    webserver updates

--- src/twitter_query.py
+++ src/twitter_query.py
@@ -1,27 +1,15 @@
 import collections
 import cPickle
-import guess_language
+import functools
 
 Tweet = collections.namedtuple('Tweet',
                                ['text','user_id','user_name',
                                 'date_tweeted','location'])
 
-def process_tweets():
-    f1 = open('all_tweets.pkl', 'rb')
-    f2 = open('new_tweets.pkl', 'wb')
-    all_tweets = cPickle.load(f1)
-    new_tweets = []
-    for i in xrange(len(all_tweets)):
-        if i % 10000 == 0:
-            print 'Processing tweet ', i
-        try:
-            if guess_language.guessLanguage(all_tweets[i].text) == 'en':
-                new_tweets.append(all_tweets[i])
-        except Exception:
-            pass
-    f1.close()
-    cPickle.dump(new_tweets, f2)
-    f2.close()
+def load_tweets(path='all_tweets.pkl'):
+  with open(path, 'rb') as f:
+    return cPickle.load(f)
+  return None
 
-if __name__ == '__main__':
-    process_tweets()
+def search_tweets(tweets, keyword):
+  return [t for t in tweets if keyword in t.text]
--- src/webserver.py
+++ src/webserver.py
@@ -1,11 +1,36 @@
+from __future__ import print_function
+
 import bottle
+import json
+import sys
+
+import twitter_query
+
+_TWEETS = None
 
 @bottle.route('/tos/hello')
 def hello():
   return 'Hello, World!'
 
+@bottle.route('/tos/tweets/<keyword:re:\w+>')
+@bottle.route('/tos/tweets/<keyword:re:\w+>/<page:int>')
+def get_tweets(keyword, page=1):
+  filtered = twitter_query.search_tweets(_TWEETS, keyword)
+  if len(filtered) <= (page-1)*10:
+    return "No more results"
+  elif len(filtered) <= page*10:
+    upper = len(filtered)
+  else:
+    upper = page*10
+  return json.dumps([t._asdict() for t in filtered[(page-1)*10:upper]])
+
 def main():
-  bottle.run(host='0.0.0.0', port=8181)
+  global _TWEETS
+  _TWEETS = twitter_query.load_tweets()
+  if not _TWEETS:
+    print("Couldn't load tweet pickle, exiting.", file=sys.stderr)
+  else:
+    bottle.run(host='localhost', port=8181)
 
 if __name__ == '__main__':
   main()
